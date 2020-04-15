6743c603c78d535ff50a70e75e963adc38099da8
umujtaba-ci <umujtaba@canadianbusinesscollege.com>
2020-04-15 18:54:30 +0000

    Initial commit

---
+++ snake.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext('2d');
+
+canvas.height = canvas.getAttribute('height');
+canvas.width = canvas.getAttribute('width');
+
+
+
+
+
+
