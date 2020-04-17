6f2d860dd8fc21b9b54c394b0fdcc406e840a300
umujtaba-ci <umujtaba@canadianbusinesscollege.com>
2020-04-17 08:45:33 +0000

    Updated scale to 25. Updated and added images based on direction. Made code ready to be responsive by using variables.

--- snake.js
+++ snake.js
@@ -1,35 +1,44 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 
-canvas.width = canvas.getAttribute('width');
-canvas.height = canvas.getAttribute('height');
+const canvasWidth = canvas.getAttribute('width');
+const canvasHeight = canvas.getAttribute('height');
+
 
 //const menuBackground = new Image();
 //menuBackground.src = "assets/img/menuBackground.jpg";
 
-//const gameBackground = new Image();
-//gameBackground.src = "assets/img/gameBackground.jpg";
+const gameBackground = new Image();
+gameBackground.src = "assets/img/gameBackground.png";
 
 const gameFood = new Image();
 gameFood.src = "assets/img/gameFood.png";
 
-const snakeHead = new Image();
-snakeHead.src = "assets/img/snakeHead.png";
+const snakeHeadX = new Image();
+snakeHeadX.src = "assets/img/snakeHeadX.png";
+
+const snakeHeadY = new Image();
+snakeHeadY.src = "assets/img/snakeHeadY.png";
 
 const snakeBody = new Image();
 snakeBody.src = "assets/img/snakeBody.png";
 
-const snakeTail = new Image();
-snakeTail.src = "assets/img/snakeTail.png";
+const snakeTailX = new Image();
+snakeTailX.src = "assets/img/snakeTailX.png";
 
-//const gameOverBanner = new Image();
-//gameOverBanner.src = "assets/img/gameOverBanner.jpg";
+const snakeTailY = new Image();
+snakeTailY.src = "assets/img/snakeTailY.png";
 
-const scale = 10;
+const gameOverBanner = new Image();
+gameOverBanner.src = "assets/img/gameOverBanner.png";
+
+const scale = 25;
+const tileCount = canvas.width/scale;
+const center = tileCount/2;
 
 let direction = {x:0, y:0};
-let position = {x:25, y:25};
-let snake = [{x:25, y:25}];
+let position = {x:center, y:center};
+let snake = [{x:center, y:center}];
 let foodPos;
 var interval;
 
@@ -37,26 +46,34 @@ function keyPress(e) {
     switch(e.keyCode) {
         case 37:
         case 65: 
-            direction = {x:-1, y:0};
+            if (direction.x !== 1) {
+                direction = {x:-1, y:0};
+            }
             break;
         case 39:
         case 68: 
-            direction = {x:1, y:0};
+            if (direction.x !== -1) {
+                direction = {x:1, y:0};
+            }
             break;
         case 38:
         case 87: 
-            direction = {x:0, y:-1};
+            if (direction.y !== 1) {
+                direction = {x:0, y:-1};
+            }
             break;
         case 40:
         case 83: 
-            direction = {x:0, y:1};
+            if (direction.y !== -1) {
+                direction = {x:0, y:1};
+            }
             break;
     }
 }
 
 function generateFood() {
-    newX = Math.floor(Math.random() * 50);
-    newY = Math.floor(Math.random() * 50);
+    newX = Math.floor(Math.random() * tileCount);
+    newY = Math.floor(Math.random() * tileCount);
     foodPos = {x:newX, y:newY};
 
     for (let element of snake) {
@@ -68,7 +85,7 @@ function generateFood() {
 }
 
 function clearBoard() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvasWidth, canvas.height);
 }
 
 function drawGameBackground() {
@@ -79,9 +96,17 @@ function drawSnake() {
     
     for (let element in snake) {
         if (element == 0) {
-            context.drawImage (snakeHead, snake[element].x * scale, snake[element].y * scale);
+            if (direction.x == 0) {
+                context.drawImage (snakeHeadY, snake[element].x * scale, snake[element].y * scale);}
+            else if (direction.y == 0){
+                context.drawImage (snakeHeadX, snake[element].x * scale, snake[element].y * scale);}
+            ;
         } else if (element == (snake.length - 1) && snake.length > 1) {
-            context.drawImage (snakeTail, snake[element].x * scale, snake[element].y * scale);
+            if (direction.x == 0) {
+                context.drawImage (snakeTailY, snake[element].x * scale, snake[element].y * scale);}
+            else if (direction.y == 0) {
+                context.drawImage (snakeTailX, snake[element].x * scale, snake[element].y * scale);}
+            ;
         } else {
             context.drawImage (snakeBody, snake[element].x * scale, snake[element].y * scale);
         }
@@ -103,14 +128,14 @@ function eatSelfCheck() {
 }
 
 function hitBorderCheck() {
-    if (position.x < 0 || position.x > canvas.width/scale || position.y < 0 || position.y > canvas.height/scale) {
+    if (position.x < 0 || position.x > tileCount || position.y < 0 || position.y > tileCount) {
         gameOver();
     }
 }
 
 function gameOver() {
     clearInterval(interval);
-    //context.drawImage (gameOverBanner, 0, 200);
+    context.drawImage (gameOverBanner, 0, (canvasWidth/2-100), canvasWidth, 200);
 }
 
 function eatFoodCheck() {
@@ -132,7 +157,7 @@ function updateSnake() {
 
 function loop() {
     clearBoard();
-    //drawGameBackground();
+    drawGameBackground();
     drawSnake();
     drawFood();
     updatePosition();
